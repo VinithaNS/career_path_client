879e51d98c02ec7cb2c2a1295f4401b166a10cdd
vinitha.G <shekar.mut@gmail.com>
2026-09-14 20:13:48 +0530

    HomeService issue commit

--- src/services/homeService.js
+++ src/services/homeService.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const getHomeData = async () => {
   try {
-    const response = await api.get("/home-page/all");
+    const response = await api.get("home-page/all");
 
     return response.data;
   } catch (error) {
